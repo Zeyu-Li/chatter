dc2d789f1f3f7e73b5a3143a983eebe560ceb45d
Zeyu-Li <zeyu.li23@gmail.com>
2021-06-08 22:23:30 -0600

    fixed unique bug

--- src/Firebase/NameCheck.js
+++ src/Firebase/NameCheck.js
@@ -3,12 +3,13 @@ import firebase from 'firebase'
 // const firestore = firebase.firestore();
 
 export const nameCheck = async (username) => {
-    return await firebase.firestore().collection('users').get(username).then(doc => {
-        console.log(doc)
+    return await firebase.firestore().collection('users').doc(username).get().then(doc => {
+        // console.log(username, doc, doc.exists)
         if (doc.exists) {
             throw "Already exists"
         }
     }).catch(err => {
+        console.log(err)
         throw "DB error"
     })
 }
