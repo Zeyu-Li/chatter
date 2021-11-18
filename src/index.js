d008d718ba1d017949517a1d0080f6e17fa3c8e5
Zeyu-Li <zeyu.li23@gmail.com>
2021-11-18 10:21:39 -0700

    fixed firebase version change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 // firebase auth
-import firebase from 'firebase/app'
-import 'firebase/analytics'
-import 'firebase/auth'
-import 'firebase/firestore'
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
 
 var firebaseConfig = {
   apiKey: "AIzaSyA1GGbvuriyk8-cii3oQJquDEFLpvj5dYU",
@@ -18,18 +18,18 @@ var firebaseConfig = {
   storageBucket: "chatter-334a3.appspot.com",
   messagingSenderId: "459734418610",
   appId: "1:459734418610:web:72170fc8a40fde017375ab",
-  measurementId: "G-V82FMJQT8M"
-}
+  measurementId: "G-V82FMJQT8M",
+};
 
 // init firebase
-firebase.initializeApp(firebaseConfig)
-firebase.analytics()
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
